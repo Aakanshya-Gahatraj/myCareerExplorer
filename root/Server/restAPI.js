fd16f524cd477d56a2a0dd9dbdd2f858f77ccba5
Aakanshya-Gahatraj <aakanshya.gahatraj707@gmail.com>
2022-07-17 06:54:07 +0545

    showing company and location

--- root/Server/restAPI.js
+++ root/Server/restAPI.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-// eslint-disable-next-line no-unused-vars
 const { jobModel, companyModel } = require("../script/dbSchemaModel");
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require("cors");
+
 app.use(express.json());
 app.use(cors());
 
@@ -19,17 +19,39 @@ mongoose
     console.log("Trouble Connecting to DB....");
   });
 
-app.get("/industry", async (req, res) => {
+app.get("/industry/:industry_name", async (req, res) => {
+  const industryName = req.params.industry_name;
   try {
-    const getIndustryData = await jobModel.find({
-      industry: "IT & Telecommunication",
-    });
+    const getIndustryData = await jobModel.find({ industry: industryName });
     // console.log("Number:", count(getIndustryData));
+    // res.send(industryName);
     res.send(getIndustryData);
   } catch (e) {
     res.status(400).send(e);
   }
 });
+
+app.get("/company", async (req, res) => {
+  const companyName = req.params.company_name;
+  try {
+    const getCompanyData = await companyModel.find({ company: companyName });
+    res.send(getCompanyData);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+app.get("/location", async (req, res) => {
+  const location = req.params.location_name;
+  try {
+    const getLocationData = await jobModel.find({ locationName: location });
+    console.log(getLocationData.count());
+    res.send(getLocationData);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server Started at Port: ${3000}`);
 });
